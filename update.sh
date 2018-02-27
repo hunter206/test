17349e8326c73b3e0058db808657b2f9638b0da9
hunter206 <770896174@qq.com>
2018-02-27 11:48:46 +0800

    update this project and run python app

--- update.sh
+++ update.sh
@@ -1,10 +1,10 @@
 #update with github
 python simMeiJi.py
 git add README.md
-#git commit -m "practice"
+git commit -m "practice"
 git add simMeiJi.py
-#git commit -m "simulate the measurment process of roadheader"
+git commit -m "simulate the measurment process of roadheader"
 git add update.sh
-#git commit -m "update this project and run python app"
+git commit -m "update this project and run python app"
 git push origin master
 
