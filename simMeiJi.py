f9112c6da94e9414d480bfe2b427e3e551da494f
hunter206 <770896174@qq.com>
2018-02-27 20:39:22 +0800

    simulate the measurment process of roadheader

--- simMeiJi.py
+++ simMeiJi.py
@@ -6,8 +6,8 @@ ROLL = 20*np.pi/180
 PITCH = 40*np.pi/180
 
 L0 = 1;
-A_b = np.matrix([0,-L0/2,0])
-B_b = np.matrix([0,L0/2,0])
+A_b = np.matrix([0,-L0/2,0]).T
+B_b = np.matrix([0,L0/2,0]).T
 
 Cbc1 = np.matrix([[0,0,1],
 		  [-1,0,0],
@@ -17,8 +17,7 @@ Cbc2 = np.matrix([[0,0,1],
 		  [0,1,0]])
 C_n = np.matrix([0,0,0]).T
 D_n = np.matrix([1,30,2]).T
-M_n = np.matrix([3,10,-4])
-
+M_n = np.matrix([3,10,-4]).T
 lBA_b = np.matrix([0,-1,0]).T
 
 R3 = np.matrix([[np.cos(YAW),-np.sin(YAW),0],
@@ -32,19 +31,29 @@ R1 = np.matrix([[1,0,0],
 		[0,np.sin(PITCH),np.cos(PITCH)]])
 Cbn = (R2*R1*R3).I
 
-Tbn = M_n.T
+Tbn = M_n
 
-A_n = Cbn*A_b.T + Tbn
-B_n = Cbn*B_b.T + Tbn
+A_n = Cbn*A_b + Tbn
+#print(A_n)
+B_n = Cbn*B_b + Tbn
+#print(B_n)
 
+M_n = np.matrix([3,10,-4])
 eAC_n = (C_n - A_n)/np.linalg.norm(C_n - A_n)
+eCD_n = (D_n - C_n)/np.linalg.norm(D_n - C_n)
 eDB_n = (B_n - D_n)/np.linalg.norm(B_n - D_n)
 lBA_n = Cbn*lBA_b
 eBA_n = lBA_n/np.linalg.norm(lBA_n)
 
 print('eAC_n=')
 print(eAC_n)
+print('eCD_n=')
+print(eCD_n)
 print('eDB_n=')
 print(eDB_n)
 print('eBA_n=')
 print(eBA_n)
+#############solving##################
+#AA = [[eCD_n],[eDB_n],[eBA_n]]
+AA = np.column_stack((np.column_stack((eCD_n,eDB_n)),eAC_n))
+print(AA)
